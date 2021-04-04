f2475fa904cbb754f5f1e516d7595cbf2594d4ad
jameskitt616 <jameskitt@posteo.de>
2021-04-04 04:46:20 +0200

    Implement initial commad line functionallity

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,45 @@
 require('../css/app.scss');
+
+$(document).ready(function () {
+    home.init();
+});
+
+let home = {};
+
+home.init = function () {
+    $('#navbarToggle').click(home.toggleNavbar);
+
+    $(window).keypress(function (e) {
+        let key = e.which;
+        if(key === 13)
+        {
+            let url = $('span#inputCommand').data('url');
+            console.log(url);
+
+            $.ajax({
+                url: url,
+                type: 'GET',
+                // data: {'searchTerm': searchTerm},
+                success: home.displayResult
+            });
+
+            return false;
+        }
+    });
+};
+
+home.displayResult = function (data) {
+
+    console.log(data)
+
+    let div = document.createElement('div');
+
+    div.innerHTML = `
+    <span class="pr-1">
+        <span class="text-green-600">haxxor@jameskitt616</span>:~$
+    </span>
+    <span>` + data + `</span>
+  `;
+
+    $('div#typingContainer').append(div)
+};
--- src/Home/Presentation/Controller/HomepageController.php
+++ src/Home/Presentation/Controller/HomepageController.php
@@ -18,4 +18,13 @@ final class HomepageController extends AbstractController
     {
         return $this->render('home/home.html.twig');
     }
+
+    /**
+     * @return Response
+     * @Route("/test", name="test")
+     */
+    public function test(): Response
+    {
+        return new Response('01100110 01101001 01101110 01100100 00100000 01100001 00100000 01101000 01101111 01100010 01100010 01111001 00100000 01100110 01101111 01110010 00100000 01100111 01101111 01100100 00100111 01110011 00100000 01110011 01100001 01101011 01100101', 200);
+    }
 }
