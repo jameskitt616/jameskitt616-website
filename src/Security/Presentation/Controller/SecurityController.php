d45597af0c7fd7978601e001ec8c9bbbaef5031a
jameskitt616 <jameskitt@posteo.de>
2021-04-28 22:57:39 +0200

    Add login action

---
+++ src/Security/Presentation/Controller/SecurityController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Security\Presentation\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @var AuthenticationUtils
+     */
+    private AuthenticationUtils $authenticationUtils;
+
+    public function __construct(AuthenticationUtils $authenticationUtils)
+    {
+        $this->authenticationUtils = $authenticationUtils;
+    }
+
+    /**
+     * @Route("/login", name="login")
+     *
+     * @return Response
+     */
+    public function login(): Response
+    {
+        $error = $this->authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $this->authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername,
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(): void
+    {
+    }
+}
