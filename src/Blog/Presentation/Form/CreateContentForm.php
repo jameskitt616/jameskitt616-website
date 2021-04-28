3b564ee84ac0f92eedc8153f3bc6d050d80f23c0
jameskitt616 <jameskitt@posteo.de>
2021-04-28 22:03:31 +0200

    Add title to content

--- src/Blog/Presentation/Form/CreateContentForm.php
+++ src/Blog/Presentation/Form/CreateContentForm.php
@@ -5,6 +5,7 @@ namespace App\Blog\Presentation\Form;
 use App\Blog\Application\Command\CreateContent;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,6 +13,10 @@ final class CreateContentForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder->add('title', TextType::class, [
+            'required' => false,
+        ]);
+
         $builder->add('text', TextareaType::class, [
             'required' => true,
         ]);
